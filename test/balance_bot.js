3320e0078b9fec9910f3078330ae0f932dc59709
Fink Viktor <finkvi@gmail.com>
2017-04-13 15:47:13 +0000

    config

--- test/balance_bot.js
+++ test/balance_bot.js
@@ -1,6 +1,8 @@
 ////////////////////////CONFIG SECTION////////////////////////////////
 const blockchainenv = process.env.BLOCKCHAINCONF || 'development';
-const botenv= process.env.BOTCONF || 'vfink_test_bot';
+const botenv= process.env.BOTCONF || 'vfink_test_bot_DEV';
+
+console.log('Starting with ENV:\nBLOCKCHAINCONF %s\nBOTCONF %s', blockchainenv, botenv);
 
 const conf = require('../conf');
 const botconf = conf.bot[botenv];
